05554e474142d39ef9ddb06e17df7b11d47d0353
Sheila Wittman <szandora789@gmail.com>
2018-06-12 21:39:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,17 @@
-class AnagramDetector 
-  attr_accessor :word 
+class Anagram
+  attr_accessor :word_to_analyze
 
-  def initialize(word)
-    @word = word 
-  end 
+  def initialize(word_to_analyze)
+    @word_to_analyze = word_to_analyze
+  end
 
-def match(anagrams)
-  puts "Must find #{self.anagrams}"
-  "#{{find_anagrams}}"
-  find_anagrams.find_all do
-    |word|
-    if word.split("").sort == 
-      self.anagrams("").sort 
-      word 
+  def match(find_anagram_in_this_string)
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word|
+      if word.split("").sort == self.word_to_analyze.split("").sort
+        word
+      end
     end
   end
+
 end
-  
