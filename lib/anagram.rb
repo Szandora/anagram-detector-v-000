183e8211d7d24bc82642e5af5ab66af0a6321af1
Sheila Wittman <szandora789@gmail.com>
2018-06-12 21:35:04 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class AnagramDetector 
+  attr_accessor :word 
+
+  def initialize(word)
+    @word = word 
+  end 
+
+def match(anagrams)
+  puts "Must find #{self.anagrams}"
+  "#{{find_anagrams}}"
+  find_anagrams.find_all do
+    |word|
+    if word.split("").sort == 
+      self.anagrams("").sort 
+      word 
+    end
+  end
+end
+  
